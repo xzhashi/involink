ea4df4da4b5bd7d651a3374ca1fe05fa274da1fb
xzhashi <shashikumartelu@gmail.com>
2025-06-30 12:51:23 +0530

    s

--- index.tsx
+++ index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client"; // The trailing slash is important here
+import { BrowserRouter } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
