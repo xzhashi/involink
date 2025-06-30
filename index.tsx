a7d713ee0661759bad217ae71c71bef9590a71ac
xzhashi <shashikumartelu@gmail.com>
2025-06-30 21:40:37 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+// Guard against SSR or other non-browser environments where `document` is not defined.
+// The app will only try to render on the client-side.
+if (typeof document !== 'undefined') {
+  const rootElement = document.getElementById('root');
+  if (!rootElement) {
+    throw new Error("Could not find root element to mount to");
+  }
 
-const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
